098347f49b45a90771de47ad4e3049f1b1ea752a
orion <orioncrocker@gmail.com>
2019-10-24 15:07:14 -0700

    bug fix. now isitclean aggressively parses lyrics rather than just compares words

--- lyricsgenius/cleancheck.py
+++ lyricsgenius/cleancheck.py
@@ -1,6 +1,6 @@
 ################################################################################
 # Author: Orion Crocker
-# Filename: check_if_clean.py
+# Filename: cleancheck.py
 # Date: 10/23/19
 #
 # Parses lyrics objects
@@ -11,19 +11,18 @@
 def check_lyrics(lyrics):
 
   # 7 words regulated by FCC with variations
-  fcc = ['shit', 'shitty', 'shitting' 'piss', 'pissy', 'pissing', 'fuck',
-         'fucky', 'fucking', 'motherfucker', 'cunt', 'cock', 'cocksucker',
-         'tits', 'titty' 'titties']
-  clean = True
+  fcc = ['shit', 'piss', 'fuck', 'cunt', 'cock',
+         'tits', 'titty', 'titties']
 
   lyric_array = lyrics.splitlines()
+
   for line in lyric_array:
+    current_line = line.lower()
     current_line = line.split()
     for word in current_line:
-      if word in fcc:
-        clean = False
+      for words in fcc:
+        if words in word:
+          print(line)
+          return 'dirty'
 
-  if clean is False:
-    return("dirty")
-  else:
-    return("clean")
+  return 'clean'
